e93818f26738c6e3e0cd3f925dff6406199b0d0f
Norhan Hamdy <dr_nora22@hotmail.com>
2024-01-10 13:08:32 -0800

    task0

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *main - main function
+ * *@ac: argc
+ * *@argv: argv
+ * *Return:int
+ * */
+int main(int ac, char **argv)
+{
+	(void)ac;
+	(void)argv;
+	loop();
+	return (0);
+}
